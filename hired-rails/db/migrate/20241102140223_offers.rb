1e36c69e8e07360b48288235895ae7201652fea1
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-11-02 15:08:05 +0100

    Revert "feat: add salary range to offer"
    This reverts commit 58bdb2bfcce2cffa8b3120067356a730b88f9003.

--- hired-rails/db/migrate/20241102140223_offers.rb
+++
@@ -1,4 +0,0 @@
-class Offers < ActiveRecord::Migration[7.2]
-  def change
-  end
-end
--- hired-rails/db/schema.rb
+++ hired-rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_02_140223) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_02_110531) do
   create_table "offers", force: :cascade do |t|
     t.string "link"
     t.string "title"
