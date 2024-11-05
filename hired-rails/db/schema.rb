7f993098f49b53c949f4e0c91e3ef3b18506b070
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-11-05 17:11:07 +0100

    feat: scaffold skills

--- hired-rails/db/schema.rb
+++ hired-rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_02_141156) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_05_155228) do
   create_table "offers", force: :cascade do |t|
     t.string "link"
     t.string "title"
@@ -20,5 +20,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_02_141156) do
     t.datetime "updated_at", null: false
     t.integer "salary_from"
     t.integer "salary_to"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
