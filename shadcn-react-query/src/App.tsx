474930251049434c9903ed0e65a5e7f42995d528
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-03-02 05:32:28 +0100

    feat: add select component

--- shadcn-react-query/src/App.tsx
+++ shadcn-react-query/src/App.tsx
@@ -1,11 +1,22 @@
 import styles from "./App.module.css"
 import { Button } from "./components/ui/button"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select"
 
 function App() {
   return (
     <div className="h-screen flex flex-col items-center justify-around">
       <div className={styles.hello}>Hello</div>
       <Button>click me</Button>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
   )
 }
