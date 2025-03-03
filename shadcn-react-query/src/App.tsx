cb96af1b95c561bc8be8665d034711686a22a5ca
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-03-03 20:28:07 +0100

    feat: install and use react query

--- shadcn-react-query/src/App.tsx
+++ shadcn-react-query/src/App.tsx
@@ -1,8 +1,42 @@
+import { useQuery } from "@tanstack/react-query"
 import styles from "./App.module.css"
 import { Button } from "./components/ui/button"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select"
+
+type ListResponse = {
+  results: {
+    name: string
+    url: string
+  }[]
+}
 
 function App() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["pokemon", "list"],
+    queryFn: () =>
+      fetch("https://pokeapi.co/api/v2/pokemon").then((res) => res.json()),
+  })
+
+  function renderSelect() {
+    if (isLoading) {
+      return null
+    }
+    const res = data! as ListResponse
+    return (
+      <SelectContent>
+        {res.results.map(({ name, url }) => (
+          <SelectItem value={url} key={url}>{name}</SelectItem>
+        ))}
+      </SelectContent>
+    )
+  }
+
   return (
     <div className="h-screen flex flex-col items-center justify-around">
       <div className={styles.hello}>Hello</div>
@@ -11,11 +45,7 @@ function App() {
         <SelectTrigger className="w-[180px]">
           <SelectValue placeholder="Theme" />
         </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
-        </SelectContent>
+        {renderSelect()}
       </Select>
     </div>
   )
